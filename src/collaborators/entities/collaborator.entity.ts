36665ad0f2443bd0bc13e24cceb58ff4282a782b
Dumar <dumarfonseka@hotmail.com>
2024-04-04 12:23:15 -0500

    update entries tools and materials

--- src/collaborators/entities/collaborator.entity.ts
+++ src/collaborators/entities/collaborator.entity.ts
@@ -24,7 +24,7 @@ export class Collaborator {
     @Column({nullable: false })
     operation: string;
 
-    @Column({ nullable: false, unique: true })
+    @Column({ nullable: false})
     document: string;
 
     @Column({ nullable: false })
